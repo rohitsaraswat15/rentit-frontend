dd20e8ee2a3339c84116f42c1e144b00393b3462
Tanya Varshney <varshneytanya192000@gmail.com>
2025-07-16 15:50:01 +0530

    fixed homepage UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,7 @@ function App() {
       <Navbar />
       <AppRoutes />
     </BrowserRouter>
-
-    {/* <div className="App" style={{ textAlign: 'center', padding: '2rem' }}> */}
-      {/* RentIt Logo */}
-      {/* <img className='w-50 h-50 m-auto'
-        src={RentItLogo}
-        alt="RentIt Logo"
-      /> */}
-        
-      {/* Heading */}
-      {/* <h1>Welcome to RentIt Frontend 🚀 by Tanya</h1>
-      <h2>Frontend Development</h2> */}
-      
-
-      {/* Info Text */}
-      {/* <p>This is a live deployed React app.</p>
-      <p>
-        Welcome to <strong>RentIt</strong>, a <strong>Graviq</strong> product — <em>"Powering the Future of Digital Decisions"</em>
-      </p>
-    </div> */}
+    
     <HomePage/>
     </>
   );
