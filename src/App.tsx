54b6db716b2c0e29ea75b07e09a06e19ce183b95
Tanya Varshney <varshneytanya192000@gmail.com>
2025-08-08 15:46:46 +0530

    fixed postproduct UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-// import RentItLogo from './assets/logo/RentIt.png';
 import './App.css';
 import Navbar from './components/common/Navbar';
 import { BrowserRouter, useLocation } from 'react-router-dom';
 import AppRoutes from './routes/AppRouter';
+import BottomHeader from './components/layout/BottomHeader';
 
 function AppWrapper() {
   return (
@@ -13,20 +13,21 @@ function AppWrapper() {
 }
 
 function App() {
-   const location = useLocation();
-    const hideNavbar =
-    location.pathname.startsWith('/admin-dashboard') ||
-    location.pathname.startsWith('/user-dashboard') ||
-    location.pathname.startsWith('/myProducts') ||
-    location.pathname.startsWith('/post-product');
-
+  const location = useLocation();
+  const hideNavbarPaths = ['/login', '/register', '/forgotpassword','/setnewpassword'];
+  const hideBottomNavbarPaths = ['/login', '/register', '/forgotpassword','/setnewpassword'];
+  
+  const hideNavbar = hideNavbarPaths.some(path => location.pathname.startsWith(path));
+  const hideBottomNavbar = hideBottomNavbarPaths.some(path => location.pathname.startsWith(path));
 
   return (
     <>
-     
-       {!hideNavbar && <Navbar />}
+
+      {!hideNavbar && <Navbar />}
+      {!hideBottomNavbar && <BottomHeader />}
+
       <AppRoutes />
-   
+
     </>
   );
 }
---
+++ src/components/layout/BottomHeader.tsx
@@ -0,0 +1,263 @@
+import React, { useEffect, useState } from 'react';
+import { GoHome, GoSearch, GoPlus } from "react-icons/go";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { LiaUserCircleSolid } from "react-icons/lia";
+import { useNavigate, useLocation, NavLink } from 'react-router-dom';
+import { FaSpinner } from 'react-icons/fa';
+import { LuLayoutDashboard, LuUpload } from "react-icons/lu";
+import { MdAccountCircle } from "react-icons/md";
+import { GoGitPullRequestDraft, GoGift } from "react-icons/go";
+import { RiMessage2Line } from "react-icons/ri";
+import { IoLogOutOutline, IoSettingsOutline } from "react-icons/io5";
+import { BsBoxes } from "react-icons/bs";
+
+
+const BottomHeader: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [user, setUser] = useState<{ name: string; role: string } | null>(null);
+  const [activeIcon, setActiveIcon] = useState<string>('home'); // Default active icon is 'home'
+  const navigate = useNavigate();
+  const [isLoggingOut, setIsLoggingOut] = useState<boolean>(false);
+  const location = useLocation();
+  const isOnDashboardPage = user && location.pathname.startsWith(`/${user.role}-dashboard`);
+
+
+  const handleLogout = () => {
+    // Show the "logging out" message
+    setIsLoggingOut(true);
+
+    // Simulate a loading delay, then handle the logout logic
+    setTimeout(() => {
+      localStorage.removeItem('user');
+      setUser(null);
+      setIsLoggingOut(false);
+      navigate('/');
+    }, 2000);
+  };
+
+  const handleLinkClick = () => {
+    // Close the sliding div when a link is clicked
+    setIsOpen(false);
+  };
+
+
+  useEffect(() => {
+    const updateUser = () => {
+      const storedUser = localStorage.getItem('user');
+      setUser(storedUser ? JSON.parse(storedUser) : null);
+    };
+
+    window.addEventListener('storage', updateUser);
+    updateUser();
+
+    return () => window.removeEventListener('storage', updateUser);
+  }, []);
+
+
+
+  const handletoggleProfile = () => {
+    setIsOpen(!isOpen)
+    if (!setIsLoggingOut) {
+      setIsOpen(false)
+    }
+  }
+
+  const handleIconClick = (icon: string) => {
+    setActiveIcon(icon);
+
+    if (!user && icon === 'post') {
+      navigate('/notsigned')
+    }
+  };
+
+  return (
+    <>
+      <div className="fixed bottom-0 left-0 right-0 bg-white border-t-2 border-gray-400 shadow-lg z-30 sm:hidden">
+        {/* This navbar will be hidden on sm and above, and visible on mobile */}
+        <div className="flex justify-between items-center py-2 px-5 cursor-pointer relative">
+
+          {!user ? (
+            <BottomHeaderLink to="/" icon={<GoHome size={26} />} label="Home" />
+          ) : (
+            isOnDashboardPage ? (
+              <BottomHeaderLink to={`/${user.role}-dashboard`} icon={<LuLayoutDashboard size={26} />} label="Dashboard" />
+
+              // <div
+              //   className={`flex flex-col items-center space-y-1 cursor-pointer ${activeIcon === 'dashboard' ? 'text-purple-500' : 'text-gray-700'}`}
+              //   onClick={() => handleIconClick('dashboard')}
+              // >
+              //   <LuLayoutDashboard size={26} />
+              //   <span className="text-xs text-gray-700">Dashboard</span>
+              //   {activeIcon === 'dashboard' && (
+              //     <div className="w-10 h-1 bg-purple-500 absolute top-0 transform-translate-x-1/2 z-10"></div>
+              //   )}
+              // </div>
+            ) : (
+              <BottomHeaderLink to="/" icon={<GoHome size={26} />} label="Home" />
+            )
+          )}
+
+
+          {/* when user on homepage */}
+          {user && !isOnDashboardPage && (
+            <>
+              <BottomHeaderLink to="/search" icon={<GoSearch size={26} />} label="Search" />
+
+              <div onClick={() => handleIconClick('post')} className="relative flex items-center justify-center bg-purple-500 text-white rounded-full p-3 transform translate-y-[-50%] shadow-xl z-30">
+                <GoPlus size={30} />
+              </div>
+            </>
+          )}
+
+          {/* when user not logged in and on homepage */}
+          {!user && (
+            <>
+              <BottomHeaderLink to="/search" icon={<GoSearch size={26} />} label="Search" />
+
+              <div onClick={() => handleIconClick('post')} className="relative flex items-center justify-center bg-purple-500 text-white rounded-full p-3 transform translate-y-[-50%] shadow-xl z-30">
+                <GoPlus size={30} />
+              </div>
+            </>
+          )}
+
+
+          {/* when user on dashborad */}
+          {user && isOnDashboardPage && (
+            <>
+              <BottomHeaderLink to="/request" icon={<GoGitPullRequestDraft size={26} />} label="Request" />
+              <BottomHeaderLink to="/postproduct" icon={<GoGift size={26} />} label="Product" />
+            </>
+          )}
+
+          {user && (
+            <BottomHeaderLink to="/messages" icon={<IoChatboxEllipsesOutline size={26} />} label="Chat" />
+          )}
+          {!user && (
+            <BottomHeaderLink to="/notsigned" icon={<IoChatboxEllipsesOutline size={26} />} label="Chat" />
+          )}
+
+          {/* Profile Icon */}
+          {!user ? (
+            <BottomHeaderLink to="/notsigned" icon={<LiaUserCircleSolid size={26} />} label="Profile" />
+          ) : (
+            <>
+              {isOnDashboardPage ? (
+                <BottomHeaderLink to="/" icon={<IoSettingsOutline size={26} />} label="Settings" />
+              ) : (
+
+                <span className="font-semibold text-gray-700 flex gap-4 items-center relative">
+                  <div
+                    className={`flex flex-col items-center space-y-1 cursor-pointer ${activeIcon === 'profile' ? 'text-purple-500' : 'text-gray-700'}`}
+                    onClick={() => handleIconClick('profile')}
+                  >
+                    <button
+                      type="button"
+                      onClick={handletoggleProfile}
+                      className="w-7 h-7 rounded-full overflow-hidden cursor-pointer flex items-center bg-red-400 justify-center text-white font-bold text-md"
+                    >
+                      {user.name
+                        .split(' ')
+                        .map((word, index, arr) =>
+                          index === 0 || index === arr.length - 1 ? word.charAt(0) : ''
+                        )
+                        .join('')}
+                    </button>
+                    <span className="text-xs text-gray-700">{user.name}</span>
+                    {activeIcon === 'profile' && (
+                      <div className="w-10 h-1 bg-purple-500 absolute top-0 transform-translate-x-1/2 z-10"></div>
+                    )}
+                  </div>
+
+                  {isOpen && (
+                    <div
+                      className={`${isOpen ? 'transform translate-y-[-5%]' : 'transform translate-y-full'
+                        } fixed bottom-[54px] left-0 w-full z-10 bg-white text-white p-4 transition-all duration-300 ease-in-out sm:hidden rounded-t-xl h-[calc(60vh-64px)] overflow-y-auto scrollbar-hidden border-2 border-t-gray-400`}
+                    >
+                      {/* Links inside the sliding div */}
+                      <div
+                        onClick={handleLinkClick}
+                        className="flex flex-col p-1 mt-2 cursor-pointer text-md text-left rounded text-gray-700 z-10"
+                      >
+                        <div
+                          onClick={() => navigate(`/${user.role}-dashboard`)}
+                          className="mt-3 hover:bg-gray-200 transition flex gap-4 items-center p-2"
+                        >
+                          <LuLayoutDashboard className="text-xl text-gray-700" /> Your Dashboard
+                        </div>
+
+                        <div className="mt-3 hover:bg-gray-200 transition flex gap-4 items-center p-2">
+                          <MdAccountCircle className="text-xl text-gray-700" /> Your Account
+                        </div>
+                        <div className="mt-3 hover:bg-gray-200 transition flex gap-4 items-center p-2">
+                          <BsBoxes className="text-xl text-gray-700" /> Your Products
+                        </div>
+                        <div className="mt-3 hover:bg-gray-200 transition flex gap-4 items-center p-2">
+                          <GoGitPullRequestDraft className="text-xl text-gray-700" /> Your Request
+                        </div>
+                        <div className="mt-3 hover:bg-gray-200 transition flex gap-4 items-center p-2">
+                          <RiMessage2Line className="text-xl text-gray-700" /> Your Message
+                        </div>
+                        <div className="mt-1 hover:bg-gray-200 transition flex gap-4 items-center p-2">
+                          <LuUpload className="text-xl text-gray-700" /> Post Product
+                        </div>
+                        <div
+                          onClick={handleLogout}
+                          className="mt-1 hover:bg-gray-200 transition flex gap-4 items-center p-2"
+                        >
+                          <IoLogOutOutline className="text-xl text-gray-700" /> Logout
+                        </div>
+                      </div>
+                    </div>
+                  )}
+
+                  {isLoggingOut && (
+                    <div className="fixed inset-0 flex justify-center items-center bg-gray-600 bg-opacity-50 z-50">
+                      <div className="bg-white p-6 rounded-lg shadow-lg text-center max-w-xs sm:max-w-sm md:max-w-md">
+                        <p className="text-lg font-semibold text-gray-800">You are logging out...</p>
+                        <div className="mt-4 animate-spin">
+                          <FaSpinner className="text-purple-500 text-3xl mx-auto" />
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </span>
+              )}
+            </>
+          )}
+
+
+        </div>
+      </div >
+    </>
+  )
+};
+
+interface BottomHeaderLinkProps {
+  to: string;
+  icon: React.ReactNode;
+  label: string;
+}
+
+const BottomHeaderLink = ({ to, icon, label }: BottomHeaderLinkProps) => {
+  const location = useLocation();
+
+  const isActive = location.pathname === to;
+
+  return (
+    <NavLink
+      to={to}
+      className={`flex flex-col items-center space-y-1 cursor-pointer ${isActive ? 'text-purple-500' : 'text-gray-700'}`}
+    >
+      <span>{icon}</span>
+      <span className="text-xs text-gray-700">{label}</span>
+      {isActive && (
+        <div className="w-10 h-1 bg-purple-500 absolute top-0 transform translate-x-1/22 z-50"></div>
+      )}
+    </NavLink>
+  )
+};
+
+
+export default BottomHeader;
+
+
